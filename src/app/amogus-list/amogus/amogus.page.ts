bb57a720dabd219265c919a971768a7065115c38
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2024-01-29 13:59:20 +0100

    bouton modif among us + image transparente liste

--- src/app/amogus-list/amogus/amogus.page.ts
+++ src/app/amogus-list/amogus/amogus.page.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AmogusPage implements OnInit {
 
+  modif: boolean = false;
+  amogus = [
+    {
+      id: 1,
+      name: "Bob",
+      color: "#ff0000",
+      img: "/assets/img/amogus.png"
+    },
+    {
+      id: 2,
+      name: "Enguerrand",
+      color: "#ffff00",
+      img: "/assets/img/amogus.png"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
