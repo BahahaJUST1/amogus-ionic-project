2a212bf67a3eaccca8a8d3faad1d1f7e3b4863f7
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2024-01-31 11:38:45 +0100

    radio sexe amogus

--- src/app/models/amogus.model.ts
+++ src/app/models/amogus.model.ts
@@ -1,9 +1,11 @@
 export class Amogus {
     id?: string;
+    sex: string;
     name: string;
     color: string;
 
     constructor() {
+        this.sex = 'E';
         this.name = '';
         this.color = '';
     }
