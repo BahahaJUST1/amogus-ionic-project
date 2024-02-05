f96faaef13e06ddac0fc9cafd9317d7fe6e20cb2
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2024-02-05 11:15:18 +0100

    ajout carte base centrée centre france leaflet

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from "leaflet";
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.page.html',
-  styleUrls: ['./map.page.scss'],
+    selector: 'app-map',
+    templateUrl: './map.page.html',
+    styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
 
-  constructor() { }
+    leafletMap: any;
+    lat: number = 47.1539;
+    lng: number = 2.2508;
+    zoom: number = 6;
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+      this.loadLeafletMap();
+    }
+
+    loadLeafletMap() {
+
+        this.leafletMap = new L.Map('leafletMap');
+        
+        const self = this;
+        
+        this.leafletMap.on("load", function () {
+            setTimeout(() => {
+                self.leafletMap.invalidateSize();
+            }, 10);
+        });
+        
+        this.leafletMap.setView([this.lat, this.lng], this.zoom);
+        
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: '&copy; <a href=”https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(this.leafletMap);
+    }
 }
